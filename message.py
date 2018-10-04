4b62411cc8ca206a722e73c377a1fbbeadc4642a
Aran Garcia <doctorarang@gmail.com>
2018-10-04 03:06:16 -0500

    nodes connect with each other

--- message.py
+++ message.py
@@ -1,20 +1,96 @@
 from enum import Enum
 
+"""
+Modulo para constantes simbolicas y funciones de construccion de mensajes.
+"""
 
-class P2PMessage(Enum):
-    """
-    Constantes para los mensajes que se utilizaran para el protocolo en la red
-    P2P.
-    """
-    LIST = 0
-    GET = 1
-    SEND = 2
-    DISC = 3
+# Header constants
+NCONNECT = 0x00  # Solicita conectarse a la red, se responde con PEERS
+PCONNECT = 0x01  # Conecta con peers individualmente después de de NCONNECT
+PEERS = 0x02
+GETFILE = 0x03
+SENDFILE = 0x04
+DISCONNECT = 0x05
+ADDME = 0x06
+ADDTHIS = 0x07
 
-class Message:
-    """docstring for Message."""
-    def __init__(self, mtype):
-        self.mtype = mtype
+# Mensajes de conexion constantes
+NCONNMSG = b"\x00\xa5\xa5\xa5\xa5"
+PCONNMSG = b"\x01\xa5\xa5\xa5\xa5"
+PROCMSG = b"\x06\xa5\xa5\xa5\xa5"
+DISCONNMSG = b"\x05\xa5\xa5\xa5\xa5"
 
-    def __bytes__(self):
-        raise NotImplementedError
+
+def build_peers_message(plist):
+    buff = bytearray()
+    buff.append(PEERS)
+    buff.extend(int.to_bytes(len(plist) * 6,
+                             length=4, byteorder="big"))
+
+    for pi in plist:
+        buff.extend(ip_to_octets_iter(pi[0]))
+        buff.extend(int.to_bytes(int(pi[1]), length=2, byteorder="big"))
+
+    return buff
+
+
+def build_addme_message(port):
+    buff = bytearray()
+    buff.append(ADDME)
+    buff.extend(int.to_bytes(port, length=2, byteorder="big"))
+
+    return buff
+
+
+def build_addthis_message(pname, pport):
+    buff = bytearray()
+    buff.append(ADDTHIS)
+    buff.extend(ip_to_octets_iter(pname))
+    buff.extend(int.to_bytes(pport, length=2, byteorder="big"))
+
+    return buff
+
+
+def ip_to_octets_iter(ip):
+    for octet in ip.split('.'):
+        yield int(octet)
+
+
+def octets_to_ip_iter(buff):
+    for octet in buff:
+        yield str(octet)
+
+
+def parse_ip_bytes(buff):
+    ips = []
+
+    pos = 0
+    end = 6
+    pbytes = buff[pos:end]
+
+    while pbytes:
+        ips.append(
+            [
+                '.'.join(octets_to_ip_iter(pbytes[:4])),
+                int.from_bytes(pbytes[4:], byteorder="big")
+            ]
+        )
+
+        pos += 6
+        end += 6
+        pbytes = buff[pos:end]
+
+    return ips
+
+
+if __name__ == '__main__':
+    # p = ["192.168.0.1:9090", "255.255.255.255:1"]
+    p = [['127.0.0.1', '8888'], ['127.0.0.1', '8889'], ['192.168.0.1', '1111']]
+
+    print([i for i in ip_to_octets_iter("192.168.0.1")])
+    print([i for i in ip_to_octets_iter("255.255.255.255")])
+    a = build_peers_message(p)
+
+    print(a)
+    print(parse_ip_bytes(a[5:]))
+    print(build_addthis_message("192.168.0.1", 8081))
--- p2p.py
+++ p2p.py
@@ -1,13 +1,24 @@
+"""
+Modulo principal para la funcionalidad P2P.
+
+Los P2PNodes son cada uno de los
+elementos pertenecientes a la red, que cuentan con la funcionalidad de atender
+y solicitar a otros nodos servicios como conexion y transferencia de archivos.
+"""
+
 import os.path
 import socket
 import threading
 
 from enum import Enum
 
+import message
+
 
 class P2Pnode:
     def __init__(self, args, sharedir='./share'):
-        self.__initcomm(int(args["port"]))
+        self.port = int(args["port"])
+        self.__initcomm(self.port)
 
         # Lista inicial vacía de los demás nodos en la red P2P
         self.peersocks = []
@@ -17,13 +28,11 @@ class P2Pnode:
         P2Pnode.__create_shared(self.sharedir)
 
         # Si no se pasan peers, una nueva red se inicia desde cero
+        self.peernames = []
         if args["peers"]:
-            self.peernames = args["peers"].split(';')
-            print("Se intentara conectar con los siguientes peers", self.peernames)
-            self.__search_peers()
+            self.__search_peers(args["peers"].split(','))
         else:
             print("No se pasaron peers. Este es el primero en la red P2P.")
-            self.peernames = []
 
     def start(self):
         while(True):
@@ -59,7 +68,7 @@ class P2Pnode:
         self.__commthread.name = "server"
         self.__commthread.start()
 
-    def __search_peers(self):
+    def __search_peers(self, peernames):
         """
         Se conecta con uno de los nodos en la red P2P. Si se pasaron las IP de mas de un
         nodo, entonces solicitara a todas hasta que conecte con solo una.
@@ -67,15 +76,27 @@ class P2Pnode:
         Al conectar con uno, recibirá la lista de todos los demás nodos.
         """
 
-        for p in self.peernames:
+        for p in peernames:
             try:
                 # Cada peer debe estar de la forma IP:PUERTO
                 peern = p.split(':')
+
+                if peern[0] == "localhost":
+                    peern[0] = "127.0.0.1"
+
+                self.peernames.append(peern)
             except ValueError as ve:
                 raise ValueError(
                     "error al parsear nodo P2P; debe ser del formato IP:PUERTO")
 
-            self.__requestconnection(peern)
+        for pn in self.peernames:
+            if self.__requestconnection(pn):
+                print("[P2P] Conexion exitosa con", pn)
+                return
+            else:
+                print("[P2P] Intento fallido de conectarse a red P2P.")
+
+        raise ValueError("no se pudo conectar con los peers proporcionados.")
 
     def __serveraccept(self):
         print("Atendiendo solicitudes...")
@@ -85,7 +106,8 @@ class P2Pnode:
 
             msg = con.recv(1024)
 
-            thr = threading.Thread(target=self.__processmsg, args=(msg,))
+            thr = threading.Thread(
+                target=self.__processmsg, args=(msg, con, addr,))
             thr.start()
 
     def __requestconnection(self, peer_con):
@@ -103,13 +125,69 @@ class P2Pnode:
         except ConnectionRefusedError:
             return False
 
-        # TODO: Envio de mensaje de a acuerdo a un protocolo
-        tmpsock.send("Me mandas peers?".encode())
+        tmpsock.send(message.NCONNMSG)
+        resp = tmpsock.recv(5)
+        if resp[0] == message.PEERS:
+            bsize = int.from_bytes(resp[1:], byteorder="big")
+            buff = tmpsock.recv(bsize)
+
+            self.peernames.extend(message.parse_ip_bytes(buff))
+            print("[CONEXION] Se actualizo la lista de peers:", self.peernames)
+
+            # Se le comparte al peer el puerto de comunicacion
+            tmpsock.send(message.build_addme_message(self.port))
+        else:
+            print("ERROR: se devolvio un mensaje no procesable.")
+            return False
 
         return True
 
-    def __processmsg(self, msg):
-        print("Procesando mensaje:", msg)
+    def __processmsg(self, msg, con, addr):
+        """
+        Procesa los 5 bytes recibidos y determina el tipo de mensaje y la accion
+        correspondiente.
+        """
+
+        msgid = msg[0]
+        if msgid == message.NCONNECT:
+            # Se conecta con un nodo y depsues con los demas
+            print("[P2P] Solicitud de conexion de", addr)
+            self.__process_connection(msg, con, addr)
+        elif msgid == message.ADDTHIS:
+            print("[P2P] Se agrego un nuevo nodo.")
+            self.__process_add(msg, con, addr)
+        else:
+            print("[P2P] Mensaje no procesable de", addr, ":\n", msg)
+
+        con.close()
+
+    def __process_connection(self, msg, con, addr):
+        con.send(message.build_peers_message(self.peernames))
+
+        # Se recibe el puerto del nuevo nodo
+        resp = con.recv(3)
+        if resp[0] == message.ADDME:
+            newport = int.from_bytes(resp[1:], byteorder="big")
+
+            # Manda a todos avisar que se agrega un nuevo nodo
+            for pn in self.peernames:
+                tmpsock = socket.socket()
+                tmpsock.connect((pn[0], int(pn[1])))
+                tmpsock.send(message.build_addthis_message(addr[0], newport))
+                tmpsock.close()
+
+            self.peernames.append((addr[0], newport))
+            print("[CONEXION] Red P2P actualizada:", self.peernames)
+
+    def __process_add(self, msg, con, addr):
+        self.peernames.append(
+            (
+                '.'.join(message.octets_to_ip_iter(msg[1:5])),
+                int.from_bytes(msg[5:7], byteorder="big")
+            )
+        )
+
+        print("[P2P] Red P2P actualizada:", self.peernames)
 
     @staticmethod
     def __create_shared(sd):
